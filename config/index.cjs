7bb2cde76678507479d855b7fcf3e8ff5ed143d8
JeromClown <119988537+JeromClown@users.noreply.github.com>
2022-12-07 16:14:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'db2a5bcc6b6d585060776a6d916df784',
 
-  PROVINCE: '山东',
-  CITY: '济南',
+  PROVINCE: '山西',
+  CITY: '晋中',
 
   USERS: [
     {
@@ -29,22 +29,22 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '王俩点', year: '2001', date: '07-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+//         {
+//           type: '生日', name: '李四', year: '1996', date: '09-31',
+//         },
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-16',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-11-23' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+//         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],

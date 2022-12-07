b34619a3f6d6920a4e4d496b9a673c5a0aa9f8a2
JeromClown <119988537+JeromClown@users.noreply.github.com>
2022-12-07 16:39:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oRDJC6TbNyII4ZIf_bpKWQm5nzIM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1ed1REyLbkZsMy7ZDGbmckoGTPAJbduVakty2NJMgI4',
+      useTemplateId: 'FZyAO1vrJhukGL1HCKlqO2J2Ep-EJPDqIR9XdbCy8q0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
